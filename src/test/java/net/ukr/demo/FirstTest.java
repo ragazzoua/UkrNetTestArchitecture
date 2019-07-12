03b1ffc5381a394aeb3cf6045cc7704c8881857a
ragazzoua <ragazzo@ukr.net>
2019-07-12 22:22:38 +0300

    change site

--- src/test/java/net/ukr/demo/FirstTest.java
+++ src/test/java/net/ukr/demo/FirstTest.java
@@ -24,58 +24,20 @@ public class FirstTest {
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-        openUkrNet();
+        openIUa();
     }
 
-    private void openUkrNet() {
-        driver.get("https://www.ukr.net/");
+    private void openIUa() {
+        driver.get("https://www.i.ua/");
     }
 
     @Test
-    public void testUkrNetOpening() {
+    public void testOpenEmail() {
 
-        goToSearch();
-        clearSearchField();
-        clickInSearchInput();
-        typeToSearchField(new SearchInput("Test"));
-        typeToSearchField(new SearchInput("Second Test"));
-        clickWeather();
-        clickExchange();
-        clickDiesel();
-        clickGoroskop();
+
     }
 
-    private void clickGoroskop() {
-        driver.findElementByXPath("/html/body/div[1]/main/div[3]/div[1]/section/ul/li[4]").click();
-    }
 
-    private void clickDiesel() {
-        driver.findElementByXPath("/html/body/div[1]/main/div[3]/div[1]/section/ul/li[3]").click();
-    }
-
-    private void clickExchange() {
-        driver.findElementByXPath("/html/body/div[1]/main/div[3]/div[1]/section/ul/li[2]").click();
-    }
-
-    private void clickWeather() {
-        driver.findElementByXPath("/html/body/div[1]/main/div[3]/div[1]/section/ul/li[1]").click();
-    }
-
-    private void typeToSearchField(SearchInput searchInput) {
-        driver.findElement(By.id("search-input")).sendKeys(searchInput.getText());
-    }
-
-    private void clickInSearchInput() {
-        driver.findElement(By.id("search-input")).click();
-    }
-
-    private void clearSearchField() {
-        driver.findElement(By.id("search-input")).clear();
-    }
-
-    private void goToSearch() {
-        driver.findElement(By.id("search-input"));
-    }
 
     @AfterMethod
     public void tearDown() {
