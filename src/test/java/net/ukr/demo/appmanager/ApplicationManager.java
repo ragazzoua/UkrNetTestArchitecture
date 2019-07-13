fe0874cefd4b2a9c1f362ccfefbde083c3ae7122
ragazzoua <ragazzo@ukr.net>
2019-07-13 14:28:17 +0300

    several methods move to the group helper

--- src/test/java/net/ukr/demo/appmanager/ApplicationManager.java
+++ src/test/java/net/ukr/demo/appmanager/ApplicationManager.java
@@ -13,7 +13,6 @@ import java.util.concurrent.TimeUnit;
  */
 
 public class ApplicationManager extends GroupHelper {
-    ChromeDriver driver;
 
     public static boolean isAlertPresent(ChromeDriver driver) {
         try {
@@ -41,22 +40,6 @@ public class ApplicationManager extends GroupHelper {
         driver.findElementByXPath("//input[@name='pass']").clear();
         driver.findElementByXPath("//input[@name='pass']").sendKeys(password);
         driver.findElementByXPath("//input[@value='Войти']").click();
-    }
-
-    public void submitGroupCreation() {
-        driver.findElementByXPath("//input[@ct='CreateGroup']").click();
-    }
-
-    public void fillGroupForm(GroupData groupData) {
-        driver.findElementByXPath("//*[@id='workBody']/div[2]/div[1]/div[2]/div[3]/ul/li[1]/p/input[1]").sendKeys(groupData.getGroupName());
-    }
-
-    public void selectGroupNameField() {
-        driver.findElementByXPath("//*[@id='workBody']/div[2]/div[1]/div[2]/div[3]/ul/li[1]/p/input[1]").click();
-    }
-
-    public void clickAddNewGroup() {
-        driver.findElementByXPath("//*[@id='add_group']/i").click();
     }
 
     public void goToContacts() {
--- src/test/java/net/ukr/demo/appmanager/GroupHelper.java
+++ src/test/java/net/ukr/demo/appmanager/GroupHelper.java
@@ -1,8 +1,28 @@
 package net.ukr.demo.appmanager;
 
+import net.ukr.demo.model.GroupData;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 /**
  * created by FAMILY 13.07.2019
  */
 
 public class GroupHelper {
+    ChromeDriver driver;
+
+    public void submitGroupCreation() {
+        driver.findElementByXPath("//input[@ct='CreateGroup']").click();
+    }
+
+    public void fillGroupForm(GroupData groupData) {
+        driver.findElementByXPath("//*[@id='workBody']/div[2]/div[1]/div[2]/div[3]/ul/li[1]/p/input[1]").sendKeys(groupData.getGroupName());
+    }
+
+    public void selectGroupNameField() {
+        driver.findElementByXPath("//*[@id='workBody']/div[2]/div[1]/div[2]/div[3]/ul/li[1]/p/input[1]").click();
+    }
+
+    public void clickAddNewGroup() {
+        driver.findElementByXPath("//*[@id='add_group']/i").click();
+    }
 }
