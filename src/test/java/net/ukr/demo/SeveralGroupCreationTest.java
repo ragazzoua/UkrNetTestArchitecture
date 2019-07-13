aef69582e5d1eaee3e24fa345c96143b831fa5f9
ragazzoua <ragazzo@ukr.net>
2019-07-13 09:38:35 +0300

    add and delete teardown method and isAlertPresent

--- src/test/java/net/ukr/demo/SeveralGroupCreationTest.java
+++ src/test/java/net/ukr/demo/SeveralGroupCreationTest.java
@@ -2,7 +2,9 @@ package net.ukr.demo;
 
 import io.github.bonigarcia.wdm.DriverManagerType;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -34,5 +36,19 @@ public class SeveralGroupCreationTest extends TestBase {
         driver.findElementByXPath("//*[@id='workBody']/div[2]/div[1]/div[2]/div[3]/ul/li[1]/p/input[1]").click();
         driver.findElementByXPath("//*[@id='workBody']/div[2]/div[1]/div[2]/div[3]/ul/li[1]/p/input[1]").sendKeys("groupname1");
         driver.findElementByXPath("//input[@ct='CreateGroup']").click();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+
+    public static boolean isAlertPresent(ChromeDriver driver) {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
     }
 }
