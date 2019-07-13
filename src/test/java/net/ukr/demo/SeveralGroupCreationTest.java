d9752d3a8de53ecf3cd22d06f9a36dd94ce1b9a5
ragazzoua <ragazzo@ukr.net>
2019-07-13 09:35:08 +0300

    delete driver because he is present in the test base class

--- src/test/java/net/ukr/demo/SeveralGroupCreationTest.java
+++ src/test/java/net/ukr/demo/SeveralGroupCreationTest.java
@@ -14,7 +14,7 @@ import java.util.concurrent.TimeUnit;
 
 public class SeveralGroupCreationTest extends TestBase {
 
-    ChromeDriver driver;
+
 
     @BeforeMethod
     public void setUp() throws Exception {
