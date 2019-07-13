90cd3498c73bc971adbf20c90362c7ce93953f66
ragazzoua <ragazzo@ukr.net>
2019-07-13 07:55:24 +0300

    move login to the before method

--- src/test/java/net/ukr/demo/FirstTest.java
+++ src/test/java/net/ukr/demo/FirstTest.java
@@ -2,7 +2,6 @@ package net.ukr.demo;
 
 import io.github.bonigarcia.wdm.DriverManagerType;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
@@ -24,12 +23,6 @@ public class FirstTest {
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-
-    }
-
-
-    @Test
-    public void groupCreationTest() {
         driver.findElementByXPath("//input[@name='login']").click();
         driver.findElementByXPath("//input[@name='login']").clear();
         driver.findElementByXPath("//input[@name='login']").sendKeys("ittest2");
@@ -37,6 +30,12 @@ public class FirstTest {
         driver.findElementByXPath("//input[@name='pass']").clear();
         driver.findElementByXPath("//input[@name='pass']").sendKeys("337774a");
         driver.findElementByXPath("//input[@value='Войти']").click();
+    }
+
+
+    @Test
+    public void groupCreationTest() {
+
         //driver.findElementByXPath("//p[@class='make_message']/a").click();
         driver.findElementByXPath("/html/body/div[1]/div[5]/ul/li[3]/a").click();
         driver.findElementByXPath("//*[@id='add_group']/i").click();
