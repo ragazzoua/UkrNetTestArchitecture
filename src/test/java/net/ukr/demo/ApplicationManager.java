1f2e9b0cfd28313856e9aca0a370807501032906
ragazzoua <ragazzo@ukr.net>
2019-07-13 10:16:26 +0300

    rename protected to public

--- src/test/java/net/ukr/demo/ApplicationManager.java
+++ src/test/java/net/ukr/demo/ApplicationManager.java
@@ -23,7 +23,7 @@ public class ApplicationManager {
         }
     }
 
-    protected void init() {
+    public void init() {
         WebDriverManager.getInstance(DriverManagerType.CHROME).setup();
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
@@ -42,27 +42,27 @@ public class ApplicationManager {
         driver.findElementByXPath("//input[@value='Войти']").click();
     }
 
-    protected void submitGroupCreation() {
+    public void submitGroupCreation() {
         driver.findElementByXPath("//input[@ct='CreateGroup']").click();
     }
 
-    protected void fillGroupForm(GroupData groupData) {
+    public void fillGroupForm(GroupData groupData) {
         driver.findElementByXPath("//*[@id='workBody']/div[2]/div[1]/div[2]/div[3]/ul/li[1]/p/input[1]").sendKeys(groupData.getGroupName());
     }
 
-    protected void selectGroupNameField() {
+    public void selectGroupNameField() {
         driver.findElementByXPath("//*[@id='workBody']/div[2]/div[1]/div[2]/div[3]/ul/li[1]/p/input[1]").click();
     }
 
-    protected void clickAddNewGroup() {
+    public void clickAddNewGroup() {
         driver.findElementByXPath("//*[@id='add_group']/i").click();
     }
 
-    protected void goToContacts() {
+    public void goToContacts() {
         driver.findElementByXPath("/html/body/div[1]/div[5]/ul/li[3]/a").click();
     }
 
-    protected void stop() {
+    public void stop() {
         driver.quit();
     }
 }
