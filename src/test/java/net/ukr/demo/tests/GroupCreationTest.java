7146ae067b0404676d4defa26c6e7b84ef7c1848
ragazzoua <ragazzo@ukr.net>
2019-07-17 06:40:44 +0300

    add List to the test

--- src/test/java/net/ukr/demo/tests/GroupCreationTest.java
+++ src/test/java/net/ukr/demo/tests/GroupCreationTest.java
@@ -4,6 +4,8 @@ import net.ukr.demo.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 /**
  * created by FAMILY 07.07.2019
  */
@@ -14,13 +16,15 @@ public class GroupCreationTest extends TestBase {
     @Test
     public void groupCreationTest() {
         app.getNavigationHelper().goToContacts();
-        int before = app.getGroupHelper().getGroupCount();
+        List<GroupData> before = app.getGroupHelper().getGroupList();
+
         app.getGroupHelper().clickAddNewGroup();
         app.getGroupHelper().selectGroupNameField();
         app.getGroupHelper().fillGroupForm(new GroupData("null"));
         app.getGroupHelper().submitGroupCreation();
-        int after = app.getGroupHelper().getGroupCount();
-        Assert.assertEquals(after, before + 1);
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+
+        Assert.assertEquals(after.size(), before.size() + 1);
 
 
     }
